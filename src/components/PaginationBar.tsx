6a3e67b5ea6b6baa79072a5a946401b2b19243af
Joseph <sejokarizz@gmail.com>
2023-11-27 20:29:47 +0300

    fix code navigation link

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -13,10 +13,11 @@ export default function PaginationBar({
     const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
 
     const numberedPageItems: JSX.Element[] = [];
-    for (let page = minPage; page < maxPage; page++) {
+
+    for (let page = minPage; page <= maxPage; page++) {
         numberedPageItems.push(
             <Link
-                href={"?page" + page}
+                href={"?page=" + page}
                 key={page}
                 className={`join-item btn ${currentPage === page ? "btn-active pointer-events-none" : ""
                     }`}
@@ -25,26 +26,21 @@ export default function PaginationBar({
             </Link>
         );
     }
+
     return (
         <>
             <div className="join hidden sm:block">{numberedPageItems}</div>
             <div className="join block sm:hidden">
                 {currentPage > 1 && (
-                    <Link
-                        href={"?page" + (currentPage - 1)}
-                        className="join-item btn"
-                    >
+                    <Link href={"?page=" + (currentPage - 1)} className="join-item btn">
                         «
                     </Link>
                 )}
                 <button className="join-item btn pointer-events-none">
-                    page {currentPage}
+                    Page {currentPage}
                 </button>
                 {currentPage < totalPages && (
-                    <Link
-                        href={"?page" + (currentPage + 1)}
-                        className="join-item btn"
-                    >
+                    <Link href={"?page=" + (currentPage + 1)} className="join-item btn">
                         »
                     </Link>
                 )}
