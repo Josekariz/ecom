5f5fca85118c3a381cd11a08f4bfceca55afa2fd
Joseph <sejokarizz@gmail.com>
2023-11-29 15:58:46 +0300

    fix  metadata

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,34 +1,41 @@
 import ProductCard from '@/components/ProductCard';
 import prisma from '../../lib/db/prisma';
+import { Metadata } from 'next';
 
-interface searchPageProps {
-    searchParams: { query: string }
+interface SearchPageProps {
+    searchParams: { query: string };
 }
 
+export function generateMetadata({
+    searchParams: { query },
+}: SearchPageProps): Metadata {
+    return {
+        title: `${query}`,
+    };
+}
 
-
-//impliment pagination here
-
-
-export default async function searchPage({ searchParams: { query } }: searchPageProps) {
+export default async function SearchPage({
+    searchParams: { query },
+}: SearchPageProps) {
     const products = await prisma.product.findMany({
         where: {
             OR: [
                 { name: { contains: query, mode: "insensitive" } },
                 { description: { contains: query, mode: "insensitive" } },
-            ]
+            ],
         },
-        orderBy: { id: "desc" }
-    })
+        orderBy: { id: "desc" },
+    });
 
     if (products.length === 0) {
-        <div className='text-center'>No Product Found</div>
+        return <div className="text-center">No products found</div>;
     }
+
     return (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-            {products.map(product => (
+            {products.map((product) => (
                 <ProductCard product={product} key={product.id} />
             ))}
-        </div >
-    )
+        </div>
+    );
 }
