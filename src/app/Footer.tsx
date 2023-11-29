9434dd1a10400d921c1bd702bc931ba01b7db7c9
Joseph <sejokarizz@gmail.com>
2023-11-29 18:50:52 +0300

    resize footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,27 +1,20 @@
+import React from "react";
+
 export default function Footer() {
-    return (
-      <footer className="bg-yellow-600	 glass p-10 text-neutral-content">
-        <div className="footer m-auto max-w-7xl">
-          <div>
-            
-            <a className="link-hover link">Branding</a>
-            
-            <a className="link-hover link">Marketing</a>
-            <a className="link-hover link">Advertisement</a>
-          </div>
-          <div>
-            
-            <a className="link-hover link">About us</a>
-            <a className="link-hover link">Contact</a>
-            <a className="link-hover link">Jobs</a>
-          </div>
-          <div>
-            
-            <a className="link-hover link">Terms of use</a>
-            <a className="link-hover link">Privacy policy</a>
-            <a className="link-hover link">Cookie policy</a>
-          </div>
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-yellow-600 glass p-6 text-neutral-content">
+      <div className="container pt-4">
+        <div className="mb-4 flex justify-center">
+          {/* Your social media icons */}
         </div>
-      </footer>
-    );
-  }
+      </div>
+
+      <div className="w-full p-4 text-center">
+        ©{currentYear} Copyright:
+        <a href="https://tw-elements.com/">Ecom</a>
+      </div>
+    </footer>
+  );
+}
